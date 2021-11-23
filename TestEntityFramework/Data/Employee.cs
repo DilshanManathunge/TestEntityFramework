a953b3a80208a5cf2f9dbc4582174f5728ef2dee
Dilshan Manathunge <51945885+DilshanManathunge@users.noreply.github.com>
2021-11-23 17:22:38 +0530

    Add project files.

---
+++ TestEntityFramework/Data/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestEntityFramework.Data
+{
+    public class Employee
+    {
+        [Key]
+
+        public Guid Id{ get; set; }
+        public String FirstName { get; set; }
+        public String LastName{ get; set; }
+        public String City { get; set; }
+    }
+}
